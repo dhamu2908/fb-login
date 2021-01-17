56b75042e3829b83736dd9a0ed4d2427f92dfa97
M Dhamodharan <71106785+dhamu2908@users.noreply.github.com>
2021-01-17 19:06:49 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function loginfun(){
+	document.getElementByClassName("logindata");
+}
+function placeholderdisable()
+{
+	<input type="text">
+}
+function fun2()
+{
+	swal("You are tring to login");
+}
